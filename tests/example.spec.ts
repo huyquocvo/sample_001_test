b9eff1f570adc9b1c1d96a5de1e0b33dae058fbb
huyquocvo <quochuy29t6@gmail.com>
2026-03-16 15:23:11 +0700

    update tcs

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://practicesoftwaretesting.com/auth/login/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/practicesoftwaretesting/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://practicesoftwaretesting.com/auth/login/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
