6e3e6e7df006ac676ef69235a144f8cd2e3d2e4a
huyquocvo <quochuy29t6@gmail.com>
2026-03-16 15:41:13 +0700

    updated

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://practicesoftwaretesting.com/auth/login/');
+  await page.goto('/auth/login/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Practice Software Testing/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://practicesoftwaretesting.com/auth/login/');
+  await page.goto('/auth/login/');
 
   // Click the get started link.
   await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
