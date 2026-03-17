22466711589d56e76934e1bbe35c2de738f37473
huyquocvo <quochuy29t6@gmail.com>
2026-03-17 11:57:33 +0700

    revert

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,20 +36,6 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-     { 
-      name: "setup", 
-      testMatch: /.*\.setup\.ts/, 
-      fullyParallel: true 
-
-    },
-    {
-      name: "ui-tests",
-      use: {
-        ...devices['Desktop Chrome'],
-        //storageState: '.auth/admin.json', // Path from setup
-      },
-      dependencies: ['setup'],
-    },
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
